74181620cd48250988c9f15431ba117a0e007a6a
Thenusan08 <144269903+Thenusan08@users.noreply.github.com>
2023-12-13 13:27:30 +0100

    Add files via upload

---
+++ enregistrerbagagesgrp1.php
@@ -0,0 +1,25 @@
+<?php
+
+echo "Saisir le nombre de passagers pour le groupe : ";
+$nbPassagers = trim( fgets( STDIN ));
+
+$poidsTotal = 0 ;
+
+$numeroPassager = 1;
+
+while( $numeroPassager <= $nbPassagers ){
+
+echo "Saisir le poids (en kg) des bagages du passager"; 
+echo $numeroPassager,
+
+$poids = rtrim( fgets( STDIN ) );
+
+$poidsTotal = $poidsTotal + $poids ;
+
+$numeroPassager = $numeroPassager + 1;
+
+}
+
+echo "Le poids total des bagages pour ce groupe est de "; 
+echo $poidsTotal, "kg.\n";
+?>
